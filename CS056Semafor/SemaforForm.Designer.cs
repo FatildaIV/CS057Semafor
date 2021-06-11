6b23786425c73e74ca6d91a2c02a311f0387c928
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 14:29:49 +0200

    Initial commit

---
+++ CS056Semafor/SemaforForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace CS056Semafor
+{
+    partial class SemaforForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.semaforTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.zapnoutButton = new System.Windows.Forms.Button();
+            this.vypnoutButton = new System.Windows.Forms.Button();
+            this.rizeniZapnoutButton = new System.Windows.Forms.Button();
+            this.rizeniVypnoutButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // semaforTimer
+            // 
+            this.semaforTimer.Enabled = true;
+            this.semaforTimer.Interval = 50;
+            this.semaforTimer.Tick += new System.EventHandler(this.semaforTimer_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::CS056Semafor.Properties.Resources.svetloVypnute;
+            this.pictureBox4.Location = new System.Drawing.Point(78, 286);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CS056Semafor.Properties.Resources.svetloVypnute;
+            this.pictureBox2.Location = new System.Drawing.Point(78, 161);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CS056Semafor.Properties.Resources.svetloVypnute;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // zapnoutButton
+            // 
+            this.zapnoutButton.Location = new System.Drawing.Point(343, 173);
+            this.zapnoutButton.Name = "zapnoutButton";
+            this.zapnoutButton.Size = new System.Drawing.Size(75, 23);
+            this.zapnoutButton.TabIndex = 5;
+            this.zapnoutButton.Text = "Zapnout";
+            this.zapnoutButton.UseVisualStyleBackColor = true;
+            this.zapnoutButton.Click += new System.EventHandler(this.zapnoutButton_Click);
+            // 
+            // vypnoutButton
+            // 
+            this.vypnoutButton.Location = new System.Drawing.Point(474, 173);
+            this.vypnoutButton.Name = "vypnoutButton";
+            this.vypnoutButton.Size = new System.Drawing.Size(75, 23);
+            this.vypnoutButton.TabIndex = 6;
+            this.vypnoutButton.Text = "Vypnout";
+            this.vypnoutButton.UseVisualStyleBackColor = true;
+            this.vypnoutButton.Click += new System.EventHandler(this.vypnoutButton_Click);
+            // 
+            // rizeniZapnoutButton
+            // 
+            this.rizeniZapnoutButton.Location = new System.Drawing.Point(343, 241);
+            this.rizeniZapnoutButton.Name = "rizeniZapnoutButton";
+            this.rizeniZapnoutButton.Size = new System.Drawing.Size(75, 38);
+            this.rizeniZapnoutButton.TabIndex = 7;
+            this.rizeniZapnoutButton.Text = "Řízení Zapnout";
+            this.rizeniZapnoutButton.UseVisualStyleBackColor = true;
+            this.rizeniZapnoutButton.Click += new System.EventHandler(this.rizeniZapnoutButton_Click);
+            // 
+            // rizeniVypnoutButton
+            // 
+            this.rizeniVypnoutButton.Location = new System.Drawing.Point(474, 241);
+            this.rizeniVypnoutButton.Name = "rizeniVypnoutButton";
+            this.rizeniVypnoutButton.Size = new System.Drawing.Size(75, 38);
+            this.rizeniVypnoutButton.TabIndex = 9;
+            this.rizeniVypnoutButton.Text = "Řízení Vypnout";
+            this.rizeniVypnoutButton.UseVisualStyleBackColor = true;
+            this.rizeniVypnoutButton.Click += new System.EventHandler(this.rizeniVypnoutButton_Click);
+            // 
+            // SemaforForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rizeniVypnoutButton);
+            this.Controls.Add(this.rizeniZapnoutButton);
+            this.Controls.Add(this.vypnoutButton);
+            this.Controls.Add(this.zapnoutButton);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SemaforForm";
+            this.Text = "CS057 Semafor - Minárik David";
+            this.Load += new System.EventHandler(this.CS056Semafor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer semaforTimer;
+        private System.Windows.Forms.Button zapnoutButton;
+        private System.Windows.Forms.Button vypnoutButton;
+        private System.Windows.Forms.Button rizeniZapnoutButton;
+        private System.Windows.Forms.Button rizeniVypnoutButton;
+    }
+}
+
---
+++ CS056Semafor/SemaforForm.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS056Semafor
+{
+    public partial class SemaforForm : Form
+    {
+        public SemaforForm()
+        {
+            InitializeComponent();
+        }
+       
+
+        class Semafor
+        {
+            public enum Stav
+            {
+                Vypnuto,
+                Zapnuto
+            }
+
+            public enum Svetla
+            {
+                Stop,
+                PripravitVolno,
+                Volno,
+                PripravitStop,
+                OranzoveSviti,
+                Zhasnute
+            }
+
+            Stav semafor = Stav.Vypnuto;
+            Stav rizeni = Stav.Vypnuto;
+            Svetla svetla = Svetla.Zhasnute;
+            DateTime casPrechodu;                                       //cas prechodu do aktualniho stavu rizeni
+
+
+            TimeSpan intervalStop = new TimeSpan(0, 0, 5);
+            TimeSpan intervalVolno = new TimeSpan(0, 0, 10);
+            TimeSpan intervalPripravitVolno = new TimeSpan(0, 0, 2);
+            TimeSpan intervalPripravitStop = new TimeSpan(0, 0, 2);
+            TimeSpan intervalBlikani = new TimeSpan(0, 0, 1);
+
+            PictureBox svetloCervene;
+            PictureBox svetloOranzove;
+            PictureBox svetloZelene;
+
+            public Semafor(PictureBox svetloCervene, PictureBox svetloOranzove, PictureBox svetloZelene)
+            {
+                this.svetloCervene = svetloCervene;
+                this.svetloOranzove = svetloOranzove;
+                this.svetloZelene = svetloZelene;
+
+                casPrechodu = DateTime.Now;
+
+            }
+
+            //nastavi stav rizeni podle parametru
+            private void ZmenitStav(Svetla novyStav)
+            {
+                this.svetla = novyStav;
+                casPrechodu = DateTime.Now;                 //zapamatovat cas prechodu
+                Vykreslit();                                //zobrazit svetla podle stavu
+            }
+
+            public void Zapnout()
+            {
+                semafor = Stav.Zapnuto;
+                ZmenitStav(Svetla.OranzoveSviti);
+                //if (stavPoZapnuti == RizeniStav.Volno)
+                //{
+                //    ZmenitStav(RizeniStav.PripravitVolno);
+                //}
+                //else
+                //{
+                //    ZmenitStav(RizeniStav.PripravitStop);
+                //}
+
+            }
+            public void Vypnout()
+            {
+                semafor = Stav.Vypnuto;
+                ZmenitStav(Svetla.Zhasnute);
+           
+            }
+
+            public void RizeniZapnout()
+            {
+                rizeni = Stav.Zapnuto;
+                ZmenitStav(Svetla.Stop);
+            }
+
+            public void RizeniVypnout()
+            {
+                rizeni = Stav.Vypnuto;
+                ZmenitStav(Svetla.OranzoveSviti);
+            }
+
+            //vykresli aktualni stav semaforu (rozsviceni svetel)
+            public void Aktualizovat()
+            {
+                TimeSpan casOdPrepnuti = DateTime.Now - casPrechodu;
+
+                //stav se zmeni jen kdyz je potreba - vykresli se jen, kdyz je potreba
+                bool zmenitStav = (semafor == Stav.Zapnuto);
+
+                if (rizeni == Stav.Zapnuto)
+                {
+                    TimeSpan interval;
+                    switch (svetla)
+                    {
+                        case Svetla.Stop: interval = intervalStop; break;
+                        case Svetla.PripravitVolno: interval = intervalPripravitVolno; break;
+                        case Svetla.Volno: interval = intervalVolno; break;
+                        case Svetla.PripravitStop: interval = intervalPripravitStop; break;
+                        default: throw new Exception();
+                    }
+
+                    zmenitStav = zmenitStav && (casOdPrepnuti > interval);
+                    if (zmenitStav && svetla == Svetla.Stop)
+                    {
+                        ZmenitStav(Svetla.PripravitVolno);
+                    }
+                    else if (zmenitStav && svetla == Svetla.PripravitVolno)
+                    {
+                        ZmenitStav(Svetla.Volno);
+                    }
+                    else if (zmenitStav && svetla == Svetla.Volno)
+                    {
+                        ZmenitStav(Svetla.PripravitStop);
+                    }
+                    else if (zmenitStav && svetla == Svetla.PripravitStop)
+                    {
+                        ZmenitStav(Svetla.Stop);
+                    }
+                }
+                else
+                {
+                    zmenitStav = zmenitStav && (casOdPrepnuti > intervalBlikani);
+                    if (zmenitStav && svetla == Svetla.Zhasnute)
+                    {
+                        ZmenitStav(Svetla.OranzoveSviti);
+                    }
+                    else if (zmenitStav)
+                    {
+                        ZmenitStav(Svetla.Zhasnute);
+                    }
+
+                }
+            }
+         
+            public void Vykreslit()
+            {
+                ZhasnoutSvetla();
+
+                switch (svetla)
+                {
+                    case Svetla.Zhasnute:
+                        
+                        break;
+
+                    case Svetla.OranzoveSviti:
+                        svetloOranzove.Image = Properties.Resources.svetloZluta;
+                        break;
+
+                    case Svetla.Stop:
+                        svetloCervene.Image = Properties.Resources.svetloCervena;
+                        break;
+
+                    case Svetla.PripravitVolno:
+                        svetloCervene.Image = Properties.Resources.svetloCervena;
+                        svetloOranzove.Image = Properties.Resources.svetloZluta;
+                        break;
+
+                    case Svetla.Volno:
+                        svetloZelene.Image = Properties.Resources.svetloZelena;
+                        break;
+
+                    case Svetla.PripravitStop:
+                        svetloOranzove.Image = Properties.Resources.svetloZluta;
+                        break;
+                }
+
+            }
+            private void ZhasnoutSvetla()
+            {
+                svetloCervene.Image = Properties.Resources.svetloVypnute;
+                svetloOranzove.Image = Properties.Resources.svetloVypnute;
+                svetloZelene.Image = Properties.Resources.svetloVypnute;
+
+            }
+        }
+     
+
+
+        private void semaforTimer_Tick(object sender, EventArgs e)
+        {
+            this.semafor.Aktualizovat();
+        }
+        Semafor semafor; 
+
+        private void CS056Semafor_Load(object sender, EventArgs e)
+        {
+            this.semafor = new Semafor(pictureBox1, pictureBox2, pictureBox4);
+        }
+
+        private void zapnoutButton_Click(object sender, EventArgs e)
+        {
+            this.semafor.Zapnout();
+        }
+
+        private void vypnoutButton_Click(object sender, EventArgs e)
+        {
+            this.semafor.Vypnout();
+        }
+
+        private void rizeniVypnoutButton_Click(object sender, EventArgs e)
+        {
+            this.semafor.RizeniVypnout();
+        }
+
+        private void rizeniZapnoutButton_Click(object sender, EventArgs e)
+        {
+            this.semafor.RizeniZapnout();
+        }
+    }
+}
